52cefaf1307fd7b660596e990950dd843fae9801
abbyibarra <109445721+abbyibarra@users.noreply.github.com>
2025-02-19 00:14:34 -0500

    edited get line

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -1,12 +1,18 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const About = () => {
+/**
+* @param {*} param0 an object holding any props passed to this component from its parent component
+* @returns
+*/
+
+const About = props => {
     const [aboutContent, setAboutContent] = useState('')
+    const [error, setError] = useState('');
 
     useEffect(() => {
         axios
-            .get('http://localhost:3001/about')
+            .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/about-us`)
             .then(response => {
                 const aboutContent = response.data.about
                 setAboutContent(aboutContent)
@@ -15,15 +21,15 @@ const About = () => {
                 const errMsg = JSON.stringify(err, null, 2) // convert error object to a string so we can simply dump it to the screen
                 setError(errMsg)
             })
-    })
+    }, [])
 
     return (
-        <div>
+        <>
             <h1>{aboutContent.title}</h1>
             <p>{aboutContent.bio}</p>
             <img src={aboutContent.image} alt="About Me" style={{ maxWidth: '100%', height: 'auto' }} />
-        </div>
-      )
+        </>
+    )
 }
 
 export default About
