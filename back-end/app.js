e6d11dbec1e1957d874b7e4b25290d242520117f
abbyibarra <109445721+abbyibarra@users.noreply.github.com>
2025-02-18 22:20:54 -0500

    added route for about content

--- back-end/app.js
+++ back-end/app.js
@@ -78,5 +78,26 @@ app.post('/messages/save', async (req, res) => {
   }
 })
 
+// a route to handle fetching "about us" info
+app.get('/about-us', (req, res) => {
+  try {
+    const aboutContent = {
+      title: 'About Us',
+      bio: 'Hi! My name is Abigail Ibarra, and I am a senior at New York University studying Neural Science with minors in Computer Science, Web Programming and Applications, and Child and Adolescent Mental Health Studies.\n\nAt NYU, I have been actively involved in the Mexican Student Association (MexSA), where I previously served as Vice President and Historian, as well as Hall Council, where I served as Director of Events. I have lived on campus throughout my time at NYU, residing at Lafayette Hall since my sophomore year. For the past two years, I have worked as a Resident Assistant (RA) at Lafayette Hall, and last summer, I was a Summer Assistant at Coral Towers. With these experiences, I’ve become very familiar with NYU’s residential life.\n\nBeyond my campus involvement, I have worked as a Product Researcher at a New York City-based startup developing a debate-focused social media platform. In this role, I applied data analysis skills and deepened my understanding of social media addiction and reward systems.\n\nAcademically and professionally, I am passionate about biotechnology, particularly in the field of brain-machine interfaces.\n\nI was born and raised in Austin, Texas, with my parents and three sisters. In my free time, I enjoy exploring the city, meeting new people, playing Stardew Valley, listening to music, and hanging out with friends.',
+      image: 'https://drive.google.com/uc?export=view&id=1zjNxuJOl-uRokDVG1_NL4ncE6Cy2Xzj2',
+    }
+    res.json({
+      about: aboutContent,
+      status: 'all good',
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(400).json({
+      error: err,
+      status: 'failed to retrieve about info',
+    })
+  }
+})
+
 // export the express app we created to make it available to other modules
 module.exports = app // CommonJS export style!
